ece448cbd9121c3233ee810218379d2877e023e6
Hiyori <sus95968@gmail.com>
2025-12-21 02:08:51 +0900

    disable vercel api

--- api/whoami.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader("Cache-Control", "no-store, max-age=0");
-  const r = await fetch("https://api.ipify.org?format=json");
-  const data = await r.json();
-  res.status(200).json({ outgoingIp: data.ip });
-}
