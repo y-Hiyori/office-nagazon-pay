44d447f85520f250027813561d1ab241a5becd1c
unknown <1056g250027@pngc.hyogo-c.ed.jp>
2026-05-20 17:05:52 +0900

    更新内容

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,16 +1,11 @@
 // src/data/products.ts
-import img1 from "./商品画像/黒豆おにぎり.png";
-import img2 from "./商品画像/ハーバリウムペン.png";
-import img3 from "./商品画像/うるたばワックス.png";
-import img4 from "./商品画像/パスタ.png";
-import img5 from "./商品画像/抹茶モンブラン.png";
-import img6 from "./商品画像/雪だるま.png";
-import img7 from "./商品画像/カンデミーナ.png";
-import img8 from "./商品画像/ハリボーゴールドベア.png";
-import img9 from "./商品画像/しゃりもにグミグレープ.png";
-import img10 from "./商品画像/ポテトチップスうすしお.png";
-import img11 from "./商品画像/カップヌードル.png";
-import img12 from "./商品画像/辛ラーメン.png";
+import img1 from "./商品画像/じゃがりこサラダ.jpg";
+import img2 from "./商品画像/じゃがりこチーズ.jpg";
+import img3 from "./商品画像/じゃがりこバター.avif";
+import img4 from "./商品画像/ピュレグミぶどう.jpg";
+import img5 from "./商品画像/ピュレグミマスカット.jpg";
+import img6 from "./商品画像/ラムネ.webp";
+
 // 画像だけのマスタ
 export type ProductImageMaster = {
   id: number;        // products テーブルの id と合わせる
@@ -25,12 +20,7 @@ export const PRODUCT_IMAGES: ProductImageMaster[] = [
   { id: 4, imageData: img4 },
   { id: 5, imageData: img5 },
   { id: 6, imageData: img6 },
-  { id: 7, imageData: img7 },
-  { id: 8, imageData: img8 },
-  { id: 9, imageData: img9 },
-  { id: 10, imageData: img10 },
-  { id: 11, imageData: img11 },
-  { id: 12, imageData: img12 },
+  
 ];
 
 // id から画像パスを取る関数（なければ undefined）
