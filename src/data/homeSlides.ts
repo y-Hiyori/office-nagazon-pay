521e39270a4a1fd48ba1934bd9a2c92b1e84a668
unknown <1056g250027@pngc.hyogo-c.ed.jp>
2026-05-27 17:11:45 +0900

    デザイン修正

--- src/data/homeSlides.ts
+++ src/data/homeSlides.ts
@@ -2,9 +2,13 @@
 
 // ✅ src 配下の画像は import して使う（Vite）
 import appHero from "./商品ヒーロー/hero_app.png";
-import herbHero from "./商品ヒーロー/hero_herb.png";
-import onigiriHero from "./商品ヒーロー/hero_onigiri.png";
 import Herogame from "./商品ヒーロー/hero_game.png";
+import pureb from "./商品ヒーロー/2.png";
+import purem from "./商品ヒーロー/3.png";
+import ramune from "./商品ヒーロー/4.png";
+import jagas from "./商品ヒーロー/5.png";
+import jagac from "./商品ヒーロー/6.png";
+import jagab from "./商品ヒーロー/7.png";
 
 export type HomeSlide = {
   id: string;
@@ -26,27 +30,71 @@ export const HOME_SLIDES: HomeSlide[] = [
     link: "/how-to",
     buttonText: "使い方を見る",
     isVisible: true,
+    sortOrder: 1,
+  },
+  {
+    id: "pureb",
+    title: "ピュレグミ グレープ",
+    desc: "商品をチェック",
+    image: pureb,
+    link: "/products/4",
+    buttonText: "商品を見る",
+    isVisible: true,
     sortOrder: 2,
   },
   {
-    id: "herb",
-    title: "ハーバリウムペン",
+    id: "purem",
+    title: "ピュレグミ マスカット",
     desc: "商品をチェック",
-    image: herbHero,
-    link: "/products/2",
+    image: purem,
+    link: "/products/5",
     buttonText: "商品を見る",
     isVisible: true,
     sortOrder: 3,
   },
+
   {
-    id: "onigiri",
-    title: "黒豆おにぎり",
+    id: "ramune",
+    title: "ラムネ",
     desc: "商品をチェック",
-    image: onigiriHero,
-    link: "/products/1",
+    image: ramune,
+    link: "/products/6",
     buttonText: "商品を見る",
     isVisible: true,
     sortOrder: 4,
+  },
+
+  {
+    id: "jagas",
+    title: "じゃがりこ サラダ",
+    desc: "商品をチェック",
+    image: jagas,
+    link: "/products/1",
+    buttonText: "商品を見る",
+    isVisible: true,
+    sortOrder: 5,
+  },
+
+  {
+    id: "jagac",
+    title: "じゃがりこ チーズ",
+    desc: "商品をチェック",
+    image: jagac,
+    link: "/products/2",
+    buttonText: "商品を見る",
+    isVisible: true,
+    sortOrder: 6,
+  },
+
+  {
+    id: "jagab",
+    title: "じゃがりこ バター",
+    desc: "商品をチェック",
+    image: jagab,
+    link: "/products/3",
+    buttonText: "商品を見る",
+    isVisible: true,
+    sortOrder: 7,
   },
 
    {
@@ -57,6 +105,6 @@ export const HOME_SLIDES: HomeSlide[] = [
     link: "/game",
     buttonText: "プレイ",
     isVisible: true,
-    sortOrder: 1,
+    sortOrder: 8,
   },
 ];
